ecac02351b028894ed744ac99982a9da851418e9
Joseph Liyon PT <120583161+jliyon23@users.noreply.github.com>
2024-11-21 10:50:06 -0500

    Updated X-Token

--- axios/axiosInstance.js
+++ axios/axiosInstance.js
@@ -7,7 +7,6 @@ const axiosInstance = axios.create({
     rejectUnauthorized: false
   }),
   headers: {
-    "x-Token": "",
     Origin: "https://ktu.edu.in",
     Referer: "https://ktu.edu.in/",
     "User-Agent":
@@ -15,37 +14,30 @@ const axiosInstance = axios.create({
   },
 });
 
-// axiosInstance.interceptors.request.use((config) => {
-//   config.httpsAgent = new https.Agent({
-//     rejectUnauthorized: false // Disable SSL verification
-//   });
-//   return config;
-// });
+const getToken = async () => {
+  try {
+    const response = await axios.get("https://fetchxtoken-api.vercel.app/bypass-recaptcha");
+    return response.data.token;
+  } catch (error) {
+    console.error("Error fetching token:", error);
+    return null;
+  }
+};
 
-// const getToken = async () => {
-//   try {
-//     const response = await axios.get("https://fetchxtoken-api.vercel.app/bypass-recaptcha");
-//     return response.data.token;
-//   } catch (error) {
-//     console.error("Error fetching token:", error);
-//     return null;
-//   }
-// };
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const token = await getToken();
 
-// axiosInstance.interceptors.request.use(
-//   async (config) => {
-//     const token = await getToken();
+    if (token) {
+      config.headers["X-Token"] = token;
+      console.log(config.headers);
+    }
 
-//     if (token) {
-//       config.headers["X-Token"] = token;
-//       console.log(config.headers);
-//     }
-
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 module.exports = axiosInstance;
